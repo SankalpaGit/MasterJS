df4a7c1de3b08d2132728ffee4b4f08e3705e6ea
Sankalpa <joshisankalpa2@gmail.com>
2024-07-18 09:10:52 +0545

    2 task complete 11 remain

--- D5.js
+++ D5.js
@@ -39,13 +39,78 @@ function stringconcat(s1, s2) {
 
 stringconcat('H', 'D');
 
+//arrow function to find the sum of two numbers
 const summer = (a, b) => a + b;
 const a = 1;
 const b = 2;
 console.log(`The sum of ${a} and ${b} is ${summer(a, b)}`);
 
 
+//function to return boolean value if the string contains the spefic characters
 const containsChar = (str, char) => str.includes(char);
 
 console.log(containsChar('hello', 'e')); // Output: true
 
+//write the function to find the multiplication of two numbers with param but second will be default
+function multi(a, b=2) {
+    console.log(`the product of ${a} and ${b} is:`,a*b); 
+}
+multi(4)
+
+
+//write the function which take two parameters as name and age but age will be default
+function greet(name, age = 21) {
+    console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+}
+
+greet("Sankalpa");
+
+/*----------------------------------------------------------------------------------------------------------------------------
+                                        HIGH ORDER FUNCTION IN JAVASCRIPT
+-----------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+
+A higher order function is a function that takes one or more functions as arguments, or returns a function as its result. 
+
+====================================
+How Higher Order Functions Work ?
+====================================
+
+So, suppose I want to write a function that calculates the area and diameter of a circle. As a beginner, the first solution that comes to our mind is to write each separate function to calculate area or diameter.
+
+Aren't we write almost same function again and again with slightly different logic? Also, the functions we have written aren't reusable
+
+HOFs is concise and modular in those scenario We only need to write the logic and pass it to function and the function will do the job.
+
+*/
+
+
+
+//Higher-order function that takes a function and a number, and calls the function that many times
+
+function repeat(func, time){
+    for (var i = 0; i <time; i++) {
+        func()
+    }
+}
+
+repeat(function(){
+    console.log('sankalpa')
+},3)
+
+
+function apply(func1, func2, value){
+    return func2(func1(value))
+}
+
+function first(x){
+    return x+2
+}
+
+function second(x){
+    return x-3
+}
+
+var result = apply(first, second,3)
+console.log(result)
---
+++ D6.js
@@ -0,0 +1,8 @@
+//Day 6 : Array in the javascript
+
+let arr1= [1,2,3,4,5]
+console.log(arr1)
+
+console.log(arr1[0])
+
+console.log(arr1[arr1.length-1])
