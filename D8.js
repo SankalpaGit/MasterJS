b1317b30b539b4cb29f0f4e8e69275070e79663c
Sankalpa <joshisankalpa2@gmail.com>
2024-07-20 21:45:17 +0545

    day 8

---
+++ D8.js
@@ -0,0 +1,13 @@
+//ES6 features
+
+const person ={
+    name: 'Sankalpa',
+    age: 18
+};
+console.log(`he is ${person.name} with the ${person.age}`);
+
+const reasult=`
+Name: ${person.name}
+Age: ${person.age}
+`
+console.log(reasult);
